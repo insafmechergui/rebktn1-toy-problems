035b2edc349ff388e361fd3b6c207ea108ca2af3
insafmechergui <insaf_mechergui@outlook.com>
2019-11-25 09:55:04 +0100

    toy problem

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,15 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  //for string.length 
+   for (var i = 0; i < string.length; i++) {
+ 	//get the character
+    var character = string.charAt(i);
+    //get the character at a specific index
+    if (string.indexOf(character) == i && string.indexOf(character, i + 1) == -1) {
+      return character;
+    }
+    return "all character are repeated";
+  }
+  
 };
