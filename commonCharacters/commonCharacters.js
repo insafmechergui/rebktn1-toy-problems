935e500530c5c4875a1aec3e0379238d68a2ec47
insafmechergui <insaf_mechergui@outlook.com>
2019-11-27 09:51:34 +0100

    common character

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -14,4 +14,40 @@
 
 var commonCharacters = function(string1, string2) {
   // TODO: Your code here!
+//check in the character existe just one time
+//if character in string1 = character string2 return string = character
+var tab = {};
+var tab2 = {};
+var result = '';
+  for (var i = 0; i < string1.length; i++) {
+	  var character = string1[i];
+	  
+		if(!tab[character]) {
+			tab[character] = 1;
+		}
+		else {
+			tab[character]++;
+		}
+	}
+
+	for (var j = 0; j < string2.length; j++) {
+	  var character2 = string2[j];
+	  
+		if(!tab2[character2]) {
+			tab2[character2] = 1;
+		}
+		else {
+			tab2[character2]++;
+		}
+	}
+	for(var key in tab){
+		for(var key2 in tab2) {
+			if(tab2[key2] === 1 && tab[key] === 1) {
+				if(Object.keys(tab) = Object.keys(tab2) ) {
+					result += key;
+				}
+			}
+		}
+	}	
+	return result;
 };
