8be48fbb5a9e8c44186e97ed98861ce906aceefb
insafmechergui <insaf_mechergui@outlook.com>
2019-12-03 09:37:32 +0100

    binary search

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,6 +10,46 @@
  * console.log(index); // null
  */
 
-var binarySearch = function (array, target) {
-};
+/*var binarySearch = function (array, target) {
+	for(var i = 0 ; i < array.length; i++) {
+		if(array.includes(target)) {
+			return array.indexOf(target);
+		}
+		else {
+			return null;
+		}
+	}
+	
+};*/
 
+	//divide the array in 2
+	//if target = medium return index
+	//if target < medium 
+	//loop in the left array < medium 
+	//else loop in the right array until target = array[i]
+
+
+var binarySearch = function (array, target) {
+
+	var start = array[0];
+	var end = array.length;
+	//var result = false;
+	
+	for(var i = 0; i< array.length) {
+	
+		while (start <= end ){ 
+		  var middle = Math.floor((start + end)/2); 
+		    if (array[middle] === target) {
+		    	return array.indexOf(target); 
+		    }
+		    else if(array[middle] > target){
+		    	end = middle - 1;
+		    	return null 
+		    }
+		    else if(array[middle] < target) {
+		    	start = middle + 1;
+		    	return null;
+		    }		    
+		}	
+	}
+}
