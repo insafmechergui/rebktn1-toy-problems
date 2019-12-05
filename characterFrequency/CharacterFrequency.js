196f69fd820f14b06dd093d88844bc40f20a5262
insafmechergui <insaf_mechergui@outlook.com>
2019-12-05 09:46:31 +0100

    toy problem nit finished

--- characterFrequency/CharacterFrequency.js
+++ characterFrequency/CharacterFrequency.js
@@ -11,3 +11,43 @@ characterFrequency("mississippi")  -->  [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ 
 
 characterFrequency("") -->  [ ]
 */
+
+//for string.length
+//if obj.character exist => value = 1
+//else value++
+//=> return obj {a: 3, b: 2, c: 1}
+//push character and value in array
+//max = bigArray[0][1] 
+//max < bigArray[i][1] ? max = bigArray[i][1] 
+
+function characterFrequency(string) {
+	var obj = {};
+	var array = [];
+	var bigArray = [];
+	
+	for(var i = 0; i < string.length; i++) {
+		var character = string[i];
+		
+		if(!obj[character]){
+			obj[character] = 1
+		}
+		else {
+			obj[character]++;
+		}
+	}
+
+	var arrayOffObject = Object.keys(obj);
+	
+	for(var i = 0; i < arrayOffObject.length; i++) {
+	//	console.log("obj"+arrayOffObject)
+		for(var key in obj){
+			//console.log("key"+obj[key]);
+
+			//return all keys with value 
+			array.push(arrayOffObject[i], obj[key])
+		}
+	}
+	bigArray.push(array);
+
+	return bigArray;
+}
