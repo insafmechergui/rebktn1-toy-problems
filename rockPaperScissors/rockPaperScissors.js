e0eedee1c29446ecd4d7ad65bce5a5de45800f4c
Faresjj <jelassi.fares@gmail.com>
2019-12-25 09:42:09 +0100

    tooy problem

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -19,6 +19,38 @@
 */
 
 var rockPaperScissors = function (
-) {
-  // TODO: your solution here
-};
+  ) {
+    // TODO: your solution here
+    var possibilities = [];
+   // var result = [];
+    var player = ['R', 'P', 'S'];
+    for(var r = 0; r < player.length; r++) {
+      for(var s = 0; s < player.length; s++) {
+          for(var p = 0; p < player.length; p++) {
+            var tab = []
+            tab.push(player[r], player[s], player[p]);
+            tab.join('');
+  //console.log(tab.join(''));
+            possibilities.push(tab);
+  //result.push(possibilities);
+          }
+      }
+    }
+    return possibilities; //['RRR', 'RRP', 'RRS', etc...]
+  };
+
+
+// var rockPaperScissors = function (nbOfRound,
+//   ) {
+//     // TODO: your solution here
+//     var possibilities = [];
+//     var player = ['R', 'P', 'S'];
+//     var count = 0;
+//     for(var r = 0; r < player.length; r++) {
+//       count++
+//       possibilities.push(player[r])
+//       if(nbOfRound === count) {
+//         return true;
+//       }
+//     }
+//   };
