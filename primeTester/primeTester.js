47c439f58279a797127d99a372e9cc59d8eb4960
insafmechergui <insaf_mechergui@outlook.com>
2020-01-16 09:04:02 +0100

    toy problme finished

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -9,4 +9,17 @@ primeTester(1) // false
 
 */
 
-function primeTester(n) {}
+function primeTester(n) {
+	if(n===1){
+		return false
+	}
+	if(n===2){
+		return true
+	}
+	for(var i = 2; i < n; i++){
+		if(n%i===0){
+			return false
+		}
+	}
+	return true
+}
