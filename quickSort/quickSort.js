ba62920e065bf28445653cdecdd16bcbe422031c
insafmechergui <insaf_mechergui@outlook.com>
2020-02-13 09:21:35 +0100

    toy problem not solved

--- quickSort/quickSort.js
+++ quickSort/quickSort.js
@@ -8,8 +8,8 @@ It is a comparison sort, meaning that it can sort items of any type for which a 
 
 Quicksort uses these steps.
 
-  1. Choose any element of the array to be the pivot. There are multiple ways of selecting a pivot; explore the options and note their 
-  advantages.
+  1. Choose any element of the array to be the pivot. There are multiple ways of selecting a pivot; explore the options and note 
+  their advantages.
   2. Divide all other elements (except the pivot) into two partitions.
   3. All elements less than the pivot must be in the first partition.
   4. All elements greater than the pivot must be in the second partition.
