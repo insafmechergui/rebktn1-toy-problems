9dc3c0f94ae9c2f53519a5d7e83626f2e6bb3161
Insaf <insaf_mechergui@outlook.com>
2021-01-13 09:41:28 +0100

    nonRepeatedCharacter

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -5,7 +5,21 @@
  *   firstNonRepeatedCharacter('ABA'); // => 'B'
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
-
-var firstNonRepeatedCharacter = function(string) {
+var firstNonRepeatedCharacter = function (string) {
   // TODO: your solution here
+  var obj = {};
+  for (var i = 0; i < string.length; i++) {
+    var c = string[i];
+    if (!obj[c]) {
+      obj[c] = true;
+    } else if (obj[c]) {
+      obj[c] = false;
+    }
+  }
+  for (var i = 0; i < string.length; i++) {
+    var c = string[i];
+    if (obj[c]) {
+      return c;
+    }
+  }
 };
